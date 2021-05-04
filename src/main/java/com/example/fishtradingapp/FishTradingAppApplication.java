a0f60e607a0b39a746ace2d332e490e08b489e1e
Andrew <a.starynskyi@yahoo.com>
2021-05-03 22:48:49 -0700

    initial commit

---
+++ src/main/java/com/example/fishtradingapp/FishTradingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishtradingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishTradingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FishTradingAppApplication.class, args);
+    }
+
+}
